69652221c30a88ebc7373c788b816e6709b0a56b
nashraf <nashraf@innostark.com>
2015-02-24 17:17:09 +0500

    Added Interfaces, Models and Services Projects

---
+++ EPM.Extension.Services/TypeRegistrations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace EPM.Extension.Services
+{
+    public static class TypeRegistrations
+    {
+        /// <summary>
+        /// Register Types for Implementation
+        /// </summary>
+        public static void RegisterType(IUnityContainer unityContainer)
+        {
+           
+        }
+    }
+}
