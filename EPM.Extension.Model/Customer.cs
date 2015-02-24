a9990944155641eaa00737ab9fb4224427c35c93
nashraf <nashraf@innostark.com>
2015-02-24 17:37:12 +0500

    Added Customer and MeteringCode entities to model project

---
+++ EPM.Extension.Model/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPM.Extension.Model
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public IEnumerable<MeteringCode> MeteringCodes { get; set; } 
+    }
+}
---
+++ EPM.Extension.Model/MeteringCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPM.Extension.Model
+{
+    public class MeteringCode
+    {
+        public string Zählpunktbezeichner { get; set; }
+        public string Kurzbezeichnung { get; set; }
+        public string Anlagentyp { get; set; }
+        public string Strasse { get; set; }
+        public string PLZ { get; set; }
+        public string Ort { get; set; }
+        public string Datenversand { get; set; }
+        public string Zählverfahren { get; set; }
+        public string Messung { get; set; }
+        public string Entnahme { get; set; }
+        public string Kundenrückmeldung { get; set; }
+    }
+}
